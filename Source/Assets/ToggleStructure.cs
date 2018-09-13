9616eec7bd23fba5aecf4c0498e0762d0e1b98fd
dacerron <dacerron@gmail.com>
2018-09-13 14:35:56 -0700

    new ui

---
+++ Source/Assets/ToggleStructure.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleStructure : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject[] structures;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSelect()
+    {
+        foreach (GameObject structure in structures)
+        {
+            structure.SetActive(!structure.activeSelf);
+        }
+    }
+}
