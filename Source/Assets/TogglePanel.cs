9b9c7b5923f982c07a0d791fb82e7058b84af782
dacerron <dacerron@gmail.com>
2018-09-13 14:35:56 -0700

    new ui

---
+++ Source/Assets/TogglePanel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePanel : MonoBehaviour {
+
+    //which panel to be toggled is linked from outside
+    public GameObject target;
+    List<GameObject> panels = new List<GameObject>();
+    bool toggle;
+	// Use this for initialization
+	void Start () {
+        //find where the panels are and store a reference
+        foreach (Transform child in transform.parent)
+        {
+            Debug.Log(child.name);
+            if(child.name == "panels")
+            {
+                foreach(Transform panel in child)
+                {
+                    panels.Add(panel.gameObject);
+                }
+            }
+        }
+        toggle = false;
+	}
+	
+	// Update is called once per frame
+    //unused
+	void Update () {
+		
+	}
+
+    public void OnSelect()
+    {
+        foreach(GameObject p in panels)
+        {
+            if(p.name == target.name)
+            {
+                Animation anim = p.GetComponent<Animation>();
+                int index = 0;
+                foreach(AnimationState state in anim)
+                {
+                    if (!toggle && index == 0)
+                    {
+                        Debug.Log(state.name);
+                        anim.Play(state.name);
+                    } else if (toggle && index == 1)
+                    {
+                        Debug.Log(state.name);
+                        anim.Play(state.name);
+                    }
+                    index++;
+                }
+            }
+        }
+        toggle = !toggle;
+    }
+}
