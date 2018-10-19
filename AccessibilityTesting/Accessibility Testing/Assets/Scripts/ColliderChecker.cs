4b857afa2101ea4f19432755f8323b85fe9f6933
dacerron <dacerron@gmail.com>
2018-10-19 14:45:49 -0700

    accessibility real dev

---
+++ AccessibilityTesting/Accessibility Testing/Assets/Scripts/ColliderChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Accessibility
+{
+    public class ColliderChecker : AccComparer
+    {
+        public float ColliderMinDistance = 0.0f;
+
+        public override bool compare(GameObject item1, GameObject item2)
+        {
+            Bounds bounds1 = item1.GetComponent<Collider>().bounds;
+            Bounds bounds2 = item2.GetComponent<Collider>().bounds;
+            Vector3 point1 = bounds1.ClosestPoint(bounds2.center);
+            Vector3 point2 = bounds2.ClosestPoint(bounds1.center);
+            if(Vector3.Distance(point1, point2) < ColliderMinDistance)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override void OnCompareReady(object source, CompareEvent evnt)
+        {
+            string log = "[ACCESSIBILITY][MOBILITY][ColliderChecker]\n";
+            if (!compare(evnt.item1, evnt.item2))
+            {
+                log += evnt.item1.name + " is too close to" + evnt.item2.name + "\n";
+            }
+            Debug.Log(log);
+        }
+    }
+}
--- Source/Assets/Accessibilty/ColliderChecker.cs
+++ Source/Assets/Accessibilty/ColliderChecker.cs
@@ -2,70 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColliderChecker : MonoBehaviour {
+namespace Accessibility
+{
+    public class ColliderChecker : AccComparer
+    {
+        public float ColliderMinDistance = 0.0f;
 
-    public float ColliderMinDistance;
-    // Use this for initialization
-    void Start() {
-        Collider[] allColliders = GetComponentsInChildren<Collider>();
-        List<infringement> items = new List<infringement>();
-        foreach (Collider coll1 in allColliders)
+        public override bool compare(GameObject item1, GameObject item2)
         {
-            infringement current = new infringement(coll1.gameObject, new List<GameObject>());
-            foreach (Collider coll2 in allColliders)
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnCompareReady(object source, CompareEvent evnt)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        struct infringement
+        {
+            public GameObject focus;
+            public List<GameObject> list;
+
+            public infringement(GameObject f, List<GameObject> l)
             {
-                if (CompareDistance(coll1, coll2))
+                focus = f;
+                list = l;
+            }
+        }
+
+        bool CompareDistance(Collider coll1, Collider coll2)
+        {
+            Vector3 point1 = coll1.bounds.ClosestPoint(coll2.bounds.center);
+            Vector3 point2 = coll2.bounds.ClosestPoint(coll1.bounds.center);
+            if (Vector3.Distance(point1, point2) < ColliderMinDistance)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        void PrintItems(List<infringement> items)
+        {
+            string log = "[ACCESSIBILITY][MOBILITY][ColliderChecker]\n";
+            foreach (infringement item in items)
+            {
+                log += item.focus.name + " is too close to...\n";
+                foreach (GameObject innerItem in item.list)
                 {
-                    current.list.Add(coll2.gameObject);
+                    log += innerItem.name + "\n";
                 }
+                log += "done\n";
             }
-            items.Add(current);
+
+            Debug.Log(log);
         }
-
-        PrintItems(items);
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
-
-    struct infringement
-    {
-        public GameObject focus;
-        public List<GameObject> list;
-
-        public infringement(GameObject f, List<GameObject> l)
-        {
-            focus = f;
-            list = l;
-        }
-    }
-
-    bool CompareDistance(Collider coll1, Collider coll2)
-    {
-        Vector3 point1 = coll1.bounds.ClosestPoint(coll2.bounds.center);
-        Vector3 point2 = coll2.bounds.ClosestPoint(coll1.bounds.center);
-        if (Vector3.Distance(point1, point2) < ColliderMinDistance)
-        {
-            return true;
-        }
-        return false;
-    }
-
-    void PrintItems(List<infringement> items)
-    {
-        string log = "[ACCESSIBILITY][MOBILITY][ColliderChecker]\n";
-        foreach(infringement item in items)
-        {
-            log += item.focus.name + " is too close to...\n";
-            foreach(GameObject innerItem in item.list)
-            {
-                log += innerItem.name + "\n";
-            }
-            log += "done\n";
-        }
-
-        Debug.Log(log);
     }
 }
