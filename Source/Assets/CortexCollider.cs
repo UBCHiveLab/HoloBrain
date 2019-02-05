06e5d13146b8c72b34776892d1d0bcd482e875d6
dacerron <dacerron@gmail.com>
2019-02-05 11:06:45 -0800

    did something that made the background not be transparent anymore, trying to reset

---
+++ Source/Assets/CortexCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CortexCollider : MonoBehaviour {
+
+    public BoxCollider myCollider;
+	// Use this for initialization
+	void Start () {
+        foreach (Renderer current in this.GetComponentsInChildren<Renderer>())
+        {
+            myCollider.bounds.Encapsulate(current.bounds);
+        }
+        myCollider.center = Vector3.zero;
+
+	}
+}
