002a028b31b3ecd4025304a03d4984aa6ad36b1f
dacerron <dacerron@gmail.com>
2019-02-06 12:27:28 -0800

    our htk again

---
+++ Source/Assets/Scripts/TESTING.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TESTING : MonoBehaviour {
+	
+	public void OnSelect ()
+    {
+        Debug.Log("clicekd");
+    }
+}
