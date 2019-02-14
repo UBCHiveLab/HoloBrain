9ba764f1206d40ca93875f58fc3735fc0bccb91d
dacerron <dacerron@gmail.com>
2019-02-13 16:57:34 -0800

    ui almost done

---
+++ Source/Assets/Scripts/ButtonAppearance.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAppearance : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Sprite hoverSprite;
+    public Sprite defaultSprite;
+    public Sprite activeSprite;
+    private SpriteRenderer renderer;
+    private bool activeState;
+    
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        if (renderer == null)
+        {
+            Debug.Log("no sprite renderer on button where expected");
+        } else
+        {
+            ResetButton();
+        }
+    }
+
+    public void SetButtonHover()
+    {
+        renderer.sprite = hoverSprite;
+    }
+
+    public void SetButtonActive()
+    {
+        renderer.sprite = activeSprite;
+    }
+
+    public void ResetButton()
+    {
+        renderer.sprite = defaultSprite;
+    }
+
+    public void OnStartGaze()
+    {
+        if(activeState)
+        {
+            return;
+        }
+        SetButtonHover();
+    }
+
+    public void OnEndGaze()
+    {
+        if(activeState)
+        {
+            return;
+        }
+        ResetButton();
+    }
+}
---
+++ Source/Assets/SubMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubMenuController : MonoBehaviour {
+
+    public GameObject SubMenu;
+
+    public void OnSelect()
+    {
+        if (SubMenu != null)
+        {
+            SubMenu.SetActive(!SubMenu.activeSelf);
+        }
+    }
+}
