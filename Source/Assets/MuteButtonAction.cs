cefec52e800f92f6e6f2cc3894eb96bc61a65d8b
dacerron <dacerron@gmail.com>
2019-04-25 14:17:53 -0700

    new files

---
+++ Source/Assets/MuteButtonAction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ButtonCommands), typeof(ButtonAppearance))]
+public class MuteButtonAction : MonoBehaviour {
+
+
+    private bool mutedState;
+    private VoiceCommandPrompt prompt;
+
+    public bool IsMuted()
+    {
+        return mutedState;
+    }
+
+    private void Mute()
+    {
+        mutedState = true;
+    }
+
+    private void UnMute()
+    {
+        mutedState = false;
+    }
+
+    private void ToggleMute()
+    {
+        mutedState = !mutedState;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        prompt = GetComponent<VoiceCommandPrompt>();
+        GetComponent<ButtonCommands>().AddCommand(MuteAction());
+        mutedState = false;
+    }
+
+    private Action MuteAction()
+    {
+        return delegate
+        {
+            if (mutedState)
+            {
+                if(prompt != null)
+                {
+                    prompt.ChangePrompt("Mute");
+                }
+                UnMute();
+                GetComponent<ButtonAppearance>().ResetButton();
+            }
+            else
+            {
+                if (prompt != null)
+                {
+                    prompt.ChangePrompt("Unmute");
+                }
+                Mute();
+                GetComponent<ButtonAppearance>().SetButtonActive();
+            }
+        };
+    }
+}
