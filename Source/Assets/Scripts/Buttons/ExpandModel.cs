9e024c80d1add0bf3141b4093f2e64f2671230a3
Degly Pava <dspavap@unal.edu.co>
2021-03-11 16:11:21 -0800

    Basic Isolate Mode

---
+++ Source/Assets/Scripts/Buttons/ExpandModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Expand components of structure
+    void Expand()
+    {
+        
+    }
+
+    // Collpase components of structure
+    void Collapse()
+    {
+
+    }
+}
